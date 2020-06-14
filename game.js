c4662762a86103f39bd44d17cb3523ca34029f0b
Bezugly Misha <Bezuglyyymisha@gmail.com>
2020-06-14 14:56:35 +0300

    Add secret minigame

--- game.js
+++ game.js
@@ -1,5 +1,6 @@
 let cnv = document.getElementById('cnv');
 let ctx = cnv.getContext('2d');
+let a = document.getElementById("secret_mode");
 
 let w = window.innerWidth / 1.05; //Без этого кастыля, я не знаю как избавиться от scroolbar'а слево-направо
 let h = window.innerHeight / 1.41; //Без этого UI сверху будет мешать змейке, снизу не будет видно футер
@@ -11,29 +12,30 @@ let sw = ~~(w / s) - 1; //Кол-во квадратов по ширине
 let sh = ~~(h / s) - 1; //Кол-во квадратов по высоте
 let border = 4; //Толщина контура
 
-let dirChanged = false;
+let speed = a ? 50 : 200;
+let dirChanged = false; //Чтобы змейка сама себя не убила
 let dir = 1;
 let snake = [];
 for (let i = 0; i < 3; i++) { //Где появляется змейка в нашей игре
 	snake.push({x: ~~(sw / 2) - i, y: ~~(sh / 2)});
 }
 
-let foodX = 0;
-let foodY = 0;
-newFood();
+let foodX = 0; //Объявляем координаты еды
+let foodY = 0; //Объявляем координаты еды
+newFood(); //Вызываем первую еду
 
-function newFood() {
+function newFood() {//Каждый раз генерирует еду
 	let valid = true;
 	do {
 		foodX = ~~(Math.random() * sw);
 		foodY = ~~(Math.random() * sh);
-		valid = true;
+		valid = true; //Чтобы еда не сзодавалась в змейке
 		for (let i = 0; i < snake.length; i++) {
 			if(foodX == snake[i].x && foodY == snake[i].y) {
 				valid = false;
 			}
 		}
-	} while(!valid);
+	} while(!valid); //Если убрать восклецательный знак, еда всезда будет спавниться в змейке XD
 }
 
 function newSnake() {
@@ -44,11 +46,11 @@ function newSnake() {
 	}
 }
 
-setInterval(update, 200); //Скорость движение змейки (Меньше - быстрее)
+setInterval(update, speed); //Скорость движение змейки (Меньше - быстрее)
 document.onkeydown = keyDown;
 
 update();
-function update() {
+function update() { //Действие происходит каждые кадр
 	ctx.clearRect(0, 0, w, h);
 	ctx.fillStyle = '#1F1F1F';
 	for (let i = 0; i <= sw; i++) { //Цикл чтобы расставить квадраты по высоте
@@ -59,12 +61,15 @@ function update() {
 	}
   ctx.fillStyle = '#47d';
 	ctx.fillRect(foodX * s + border, foodY * s + border, s - border, s - border);
-  ctx.fillStyle = '#'+Math.floor(Math.random()*2**24).toString(16).padStart(6, '0');
+	if (a)
+  		ctx.fillStyle = '#'+Math.floor(Math.random()*2**24).toString(16).padStart(6, '0');//Змейка меняет всё время цвет
+	else
+		ctx.fillStyle = '#faf'//Просто розовая змейка
 	let dx = snake[0].x;
 	let dy = snake[0].y;
-	if(dx == foodX && dy == foodY) {
+	if(dx == foodX && dy == foodY) { //Если змея съела еду
 		newFood();
-		snake.push({x: snake[snake.length - 1].x, y: snake[snake.length - 1].y});
+		snake.push({x: snake[snake.length - 1].x, y: snake[snake.length - 1].y}); //Увеличиваем на один сегмент змейку
 	}
 	for (let i = 0; i < snake.length; i++) {
 		let x = snake[i].x;
@@ -75,7 +80,7 @@ function update() {
 		snake[i].x = snake[i - 1].x;
 		snake[i].y = snake[i - 1].y;
 	}
-	if(dir == 0) {
+	if(dir == 0) {//Направление змейки
 		dy--;
 	}
 	else if(dir == 1) {
@@ -87,8 +92,8 @@ function update() {
 	else {
 		dx--;
 	}
-	snake[0].x = (dx % sw + sw) % sw;
-	snake[0].y = (dy % sh + sh) % sh;
+	snake[0].x = (dx % sw + sw) % sw; //Изменение координат змейки
+	snake[0].y = (dy % sh + sh) % sh; //Изменение координат змейки
 	for (let i = 1; i < snake.length; i++) {
 		if(snake[0].x == snake[i].x && snake[0].y == snake[i].y) {
 			newFood();
@@ -100,22 +105,22 @@ function update() {
 
 function keyDown(e) {
 	if(dirChanged) return;
-	let newDir = -1;
-	switch(e.keyCode) {
+	let newDir = -1; //Если змейка перевёрнуто
+	switch(e.keyCode) {//Создаем противоположеное движение змейки
 		case 87:
-			newDir = 0;
+			newDir = 0;//Вверх (W)
 			break;
 		case 68:
-			newDir = 1;
+			newDir = 1;//Вправо (D)
 			break;
 		case 83:
-			newDir = 2;
+			newDir = 2;//Вниз (S)
 			break;
-		case 65:
-			newDir = 3;
+		case 65: 
+			newDir = 3;//Влево (A)
 			break;
 	}
-  if(newDir == -1) return;
+  if(newDir == -1) return; //Если просим змейку пойти в обратную сторону
 	if(((dir + 2) % 4) != newDir) {
 		dir = newDir;
 		dirChanged = true;
